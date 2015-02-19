f9990d60caf2354d109a19b52e1c380271826885
Nathan Cobb <nathancobb@gmail.com>
2015-02-19 15:59:40 -0800

    first commit

---
+++ event.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace TCG\Events;
+
+class Event extends \Slim\Middleware {
+
+	protected $listeners = array();
+
+	public function call(){
+
+        $app = $this->app;
+        $event = $this;
+		$app->event = $this;
+
+		// call next middleware or app
+		$this->next->call();
+	}
+
+	private function match($pattern, $name) {
+		// TODO: return true/false if pattern matches name
+		echo($this->glob2regex($name));
+	}
+
+	public function on($pattern, $callable){
+		array_push($this->listeners, array('pattern': $pattern, 'callable': $callable));
+	}
+
+	public function emit($name, $data) {
+		foreach ($this->listeners as $listener)
+			if($this->match($listener['pattern'], $name))
+				call_user_func($listener['callable'], $name, $data);
+	}
+
+	private function glob2regex($string) {
+	    return implode('\:',array_map(
+	        function($s){
+	            if($s == '\*') return '[^:]+';
+	            if($s == '\*\*') return '.*';
+	            return $s;
+	        },
+	        explode('\:', preg_quote($string))
+	    ));
+	}
+}
+
+// $app->event->on('account:order:*', function($name, $data){
+// 	// fire a pixel
+// });
+
+// $app->event->emit('account:order:success', array(
+// 	'amount' => 22.86,
+// 	'foo' => 'bar'
+// ));
