66b037334ae70d2ae87040cc796ee1b7542602a0
Mike Marcacci <mike.marcacci@gmail.com>
2015-03-06 18:07:58 -0800

    Fix broken middleware

--- src/Middleware.php
+++ src/Middleware.php
@@ -7,12 +7,12 @@ class Middleware extends \Slim\Middleware {
 	protected $emitter;
 
 	public function __construct($deliminator = ':') {
-		$this->emitter = new EventEmitter($deliminator);
+		$this->emitter = new Emitter($deliminator);
 	}
 
 	public function call() {
 		// add a colon-separated event emitter to the app
-		$this->app->events = $this->events;
+		$this->app->events = $this->emitter;
 
 		// that's all, folks!
 		$this->next->call();
